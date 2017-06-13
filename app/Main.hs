60f590077e534e424adcceb9f0f75100a95c7758
Matthew Mongeau <halogenandtoast@gmail.com>
2017-06-13 14:44:41 +0900

    Configure port with settings.yml

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 
 import Web.Spock
 import Web.Spock.Config
+import Control.Monad.IO.Class (liftIO)
+import Data.Yaml.Config (loadYamlSettings, useEnv)
+import Data.Aeson (parseJSON, FromJSON, withObject, (.:))
+
+data AppSettings = AppSettings { appPort :: Int }
+
+instance FromJSON AppSettings where
+  parseJSON = withObject "AppSettings" $ \o -> do
+    appPort <- o .: "port"
+    return AppSettings{..}
 
 main :: IO ()
 main = do
-  spockCfg <- defaultSpockCfg () PCNoDatabase ()
-  runSpock 8080 (spock spockCfg app)
+  settings <- liftIO $ loadYamlSettings ["settings.yml"] [] useEnv
+  spockCfg <- defaultSpockCfg () PCNoDatabase settings
+  runSpock (appPort settings) (spock spockCfg app)
 
-app :: SpockM () () () ()
+app :: SpockM () () AppSettings ()
 app = get root $ text "Hello World!"
