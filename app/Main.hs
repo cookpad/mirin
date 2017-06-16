4fe431de16d03ea563636ca43ea14a61aee4c3d8
Matthew Mongeau <halogenandtoast@gmail.com>
2017-06-16 16:01:18 +0900

    Connect to MySQL database

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,8 @@ import Web.Spock.Config
 import Control.Monad.IO.Class (liftIO)
 import Data.Yaml.Config (loadYamlSettings, useEnv)
 import Data.Aeson (parseJSON, FromJSON, withObject, (.:))
-import Database.Persist.MySQL (MySQLConf)
+import Database.Persist.MySQL (MySQLConf, createMySQLPool, SqlBackend, myConnInfo, myPoolSize)
+import Control.Monad.Logger (runStdoutLoggingT)
 
 data AppSettings = AppSettings { appPort :: Int
                                , appDatabaseConf :: MySQLConf
@@ -20,8 +21,11 @@ instance FromJSON AppSettings where
 main :: IO ()
 main = do
   settings <- liftIO $ loadYamlSettings ["settings.yml"] [] useEnv
-  spockCfg <- defaultSpockCfg () PCNoDatabase settings
+  pool <- runStdoutLoggingT $ createMySQLPool
+    (myConnInfo $ appDatabaseConf settings)
+    (myPoolSize $ appDatabaseConf settings)
+  spockCfg <- defaultSpockCfg () (PCPool pool) settings
   runSpock (appPort settings) (spock spockCfg app)
 
-app :: SpockM () () AppSettings ()
+app :: SpockM SqlBackend () AppSettings ()
 app = get root $ text "Hello World!"
