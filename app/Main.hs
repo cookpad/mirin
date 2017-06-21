a4e77b1d8d6980130f30c9f89bf48a3534f7c289
Matthew Mongeau <halogenandtoast@gmail.com>
2017-06-21 10:33:23 +0900

    Redirection Endpoint
    Implement wildcard endpoint to handle redirections. First we lookup the
    domain and a matching originalPath and then redirect to the
    corresponding destinationPath.

--- app/Main.hs
+++ app/Main.hs
@@ -12,26 +12,56 @@ module Main where
 -- Imports
 ------------
 
+-- Base
+import Control.Monad.IO.Class (MonadIO)
+import Data.Monoid ((<>))
+import Data.Maybe (fromMaybe)
+
 -- Aeson
 import Data.Aeson ((.:), FromJSON)
 import qualified Data.Aeson as A
 
+-- HTTP
+import Network.HTTP.Types.Status (status301)
+
 -- Logging
+import Control.Monad.Logger (LoggingT)
 import qualified Control.Monad.Logger as L
 
 -- Mysql
-import Database.Persist.MySQL (MySQLConf)
+import Database.Persist.MySQL (MySQLConf, Entity(..), (==.))
 import qualified Database.Persist.MySQL as MS
 
 -- Persistent
 import qualified Database.Persist.TH as P
  
 -- Spock
-import Web.Spock (SpockM, spock, runSpock, get, root, text)
+import Network.Wai.Internal (Request, requestHeaderHost)
+import Web.Spock ( SpockM
+                 , HasSpock
+                 , SpockConn
+                 , spock
+                 , runSpock
+                 , get
+                 , root
+                 , html
+                 , wildcard
+                 , ActionT
+                 , runQuery
+                 , setStatus
+                 , setHeader
+                 , getState
+                 , request
+                 )
 import Web.Spock.Config (defaultSpockCfg, PoolOrConn(..))
 
 -- Text
 import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8)
+
+-- Transformers
+import Control.Monad.Trans.Maybe (MaybeT(..), runMaybeT)
+import Control.Monad.Trans.Reader (ReaderT)
 
 -- Yaml
 import qualified Data.Yaml.Config as Y
@@ -42,17 +72,24 @@ import qualified Data.Yaml.Config as Y
 ------------
 data AppSettings = AppSettings { appPort :: Int
                                , appDatabaseConf :: MySQLConf
+                               , appBase :: Text
                                }
 
 instance FromJSON AppSettings where
   parseJSON = A.withObject "AppSettings" $ \json -> AppSettings
     <$> json .: "port"
     <*> json .: "database"
+    <*> json .: "base-url"
 
 
 ------------
 -- Database
 ------------
+type DB = ReaderT MS.SqlBackend (LoggingT IO)
+
+runSQL :: (HasSpock m, SpockConn m ~ MS.SqlBackend) => MS.SqlPersistT (LoggingT IO) a -> m a
+runSQL action = runQuery $ \conn -> L.runStdoutLoggingT $ MS.runSqlConn action conn
+
 P.share
   [P.mkPersist P.sqlSettings, P.mkMigrate "migrateAll"]
   [P.persistLowerCase|
@@ -63,10 +100,28 @@ P.share
       DomainRedirect domain originalPath
   |]
 
+getRedirection :: UrlPath -> Domain -> DB (Maybe (Entity Redirect))
+getRedirection path domain = MS.selectFirst cond []
+  where cond = [ RedirectDomain ==. domainText domain
+               , RedirectOriginalPath ==. path
+               ]
+
 
 ------------
 -- Spock
 ------------
+type UrlPath = Text
+newtype Domain = Domain { domainText :: Text }
+
+redirect301 :: MonadIO m => Text -> ActionT m ()
+redirect301 url = do
+    setStatus status301
+    setHeader "Location" url
+    html $ "<html><body>You are being <a src=\"" <> url <> "\">redirected</a>.</body></html>"
+
+getDomain :: Request -> Maybe Domain
+getDomain = (Domain . decodeUtf8 <$>) . requestHeaderHost
+
 main :: IO ()
 main = do
   settings@AppSettings{..} <- Y.loadYamlSettings ["settings.yml"] [] Y.useEnv
@@ -78,4 +133,10 @@ main = do
   runSpock appPort (spock spockCfg app)
 
 app :: SpockM MS.SqlBackend () AppSettings ()
-app = get root $ text "Hello World!"
+app = get wildcard $ \path -> do
+  rootUrl <- appBase <$> getState
+  mpath <- runMaybeT $ do
+    domain <- MaybeT (getDomain <$> request)
+    redirection <- MaybeT (runSQL $ getRedirection path domain)
+    return . redirectDestinationPath . entityVal $ redirection
+  redirect301 $ rootUrl <> fromMaybe "" mpath
